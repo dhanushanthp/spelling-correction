8d3e8db0442fd6286332872272019c3dfa58cd33
Dhanushanth <dhanushanthp@gmail.com>
2015-08-12 16:20:34 +0530

    Initial commit

---
+++ src/cs276/pe1/lucene/IMDBIndexer.java
@@ -0,0 +1,48 @@
+package cs276.pe1.lucene;
+
+import java.io.File;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.util.Version;
+
+import cs276.pe1.lucene.IMDBParser.MoviePlotRecord;
+
+public class IMDBIndexer {
+
+	public static void main(String[] argv) throws Exception {
+
+		File indexPath = getIndexPath();
+		indexPath.mkdir(); 
+
+		@SuppressWarnings("deprecation")
+		IndexWriter writer = new IndexWriter(indexPath, getLuceneAnalyzer(), true, IndexWriter.MaxFieldLength.LIMITED);
+		Document d;
+		int cnt = 0;
+		for (MoviePlotRecord rec : IMDBParser.readRecords()) { // index each record
+
+			cnt++;
+		}
+		writer.close();
+		System.out.println("Added a total of " + cnt + " records to the index.");
+		System.err.println("Done"); 
+	}
+
+	/**
+	 * Get the correct analyzer to use when instantiating a QueryParser.
+	 * 
+	 * @return the analyzer used to build the index 
+	 */
+	public static Analyzer getLuceneAnalyzer() {
+		return new StandardAnalyzer(Version.LUCENE_CURRENT);
+	}
+
+	/**
+	 * @return the path of the IMDB index
+	 */
+	public static File getIndexPath() {
+		return new File(new File(System.getProperty("user.home")),"cs276-index");
+	}
+}
